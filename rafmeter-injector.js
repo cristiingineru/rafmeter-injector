e88e42d9908fa75e0f14f7725bcd1886308b44a4
Cristi Ingineru <cristi.ingineru@gmail.com>
2017-02-17 22:28:47 +0200

    First version

---
+++ rafmeter-injector.js
@@ -0,0 +1,31 @@
+var jsdom = require('jsdom');
+var rafmeter = require('rafmeter');
+var fs = require('fs');
+
+
+var insertFirst = function(parent, newChild) {
+  if (parent.firstChild) {
+    parent.insertBefore(newChild, parent.firstChild);
+  } else {
+    parent.appendChild(newChild);
+  }
+};
+
+
+jsdom.env(
+  "https://iojs.org/dist/",
+  [],
+  function (err, window) {
+    var instrumentation = 'var RAFMeter = ' + rafmeter.toString() + '\n' + 'window.rafMeter = new RAFMeter();',
+        scriptElement = window.document.createElement('script'),
+        contentNode = window.document.createTextNode(instrumentation);
+
+    scriptElement.appendChild(contentNode);
+    insertFirst(window.document.body, scriptElement)
+
+    fs.writeFile('out.html', window.document.documentElement.outerHTML,
+      function (error){
+          if (error) throw error;
+      });
+  }
+);
